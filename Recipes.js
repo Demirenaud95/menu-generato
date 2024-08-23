9c2f26c518e2d1b4382415de8d9f8c20be313bed
Demirenaud95 <demi.renaud95@outlook.com>
2024-08-23 11:34:28 -0400

    Create Recipes.js
    Ajout du fichier Recipes.js

---
+++ Recipes.js
@@ -0,0 +1,18 @@
+module.exports = {
+  breakfast: [
+    { name: "Omelette aux légumes", calories: 250, protein: 20, carbs: 5, fats: 18, tags: ["vegetarien", "sans gluten"] },
+    { name: "Smoothie aux fruits", calories: 300, protein: 15, carbs: 50, fats: 5, tags: ["vegetarien", "sans gluten", "vegan"] },
+  ],
+  lunch: [
+    { name: "Salade de quinoa", calories: 400, protein: 15, carbs: 60, fats: 15, tags: ["vegetarien", "sans gluten", "vegan"] },
+    { name: "Poulet grillé avec riz", calories: 600, protein: 40, carbs: 70, fats: 20, tags: ["sans gluten"] },
+  ],
+  dinner: [
+    { name: "Poisson grillé avec légumes", calories: 450, protein: 35, carbs: 30, fats: 15, tags: ["sans gluten"] },
+    { name: "Pâtes complètes", calories: 500, protein: 20, carbs: 80, fats: 10, tags: ["vegetarien"] },
+  ],
+  snacks: [
+    { name: "Noix", calories: 200, protein: 5, carbs: 10, fats: 18, tags: ["vegetarien", "sans gluten", "vegan"] },
+    { name: "Yaourt grec", calories: 150, protein: 10, carbs: 10, fats: 5, tags: ["vegetarien", "sans gluten"] },
+  ],
+};
